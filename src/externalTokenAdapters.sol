ea3c17923e452da0c11fdbe12153631a60e62d7a
Joseph Krug <joeykrug@gmail.com>
2019-04-29 15:10:19 -0700

    Update externalTokenAdapters.sol

--- src/externalTokenAdapters.sol
+++ src/externalTokenAdapters.sol
@@ -2,9 +2,9 @@
 
 // Copyright (C) 2018 Rain <rainbreak@riseup.net>
 //
-// This program is transferCollateralFromCDP software: you can redistribute it and/or modify
+// This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
-// the transferCollateralFromCDP Software Foundation, either version 3 of the License, or
+// the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
 // This program is distributed in the hope that it will be useful,
